10ab87e8bd784903717142dc3f8b5731360b1a82
tye-shutty <shuttyt@gmail.com>
2021-03-21 10:07:00 -0300

    Add folder mysql

--- signin.py
+++ signin.py
@@ -12,75 +12,75 @@ import ssl #include ssl libraries
 import sys
 
 class SignIn(Resource):
-	#curl -i -H "Content-Type: application/json" -X POST -d '{"username": "tshutty", "password": "..."}' -c cookie-jar -b cookie-jar -k https://cs3103.cs.unb.ca:5045/signin
-	def post(self):
-		print('initial data\nsid=',session.sid,'session=',session)
-		print('cookie=', request.cookies)
-		# print('request=', request.headers)
+    #curl -i -H "Content-Type: application/json" -X POST -d '{"username": "tshutty", "password": "..."}' -c cookie-jar -b cookie-jar -k https://cs3103.cs.unb.ca:5045/signin
+    def post(self):
+        print('initial data\nsid=',session.sid,'session=',session)
+        print('cookie=', request.cookies)
+        # print('request=', request.headers)
 
-		if not request.json:
-			abort(400) # bad request
+        if not request.json:
+            abort(400) # bad request
 
-		# Parse the json
-		parser = reqparse.RequestParser()
-		try:
- 			# Check for required attributes in json document, create a dictionary
-			parser.add_argument('username', type=str, required=True)
-			parser.add_argument('password', type=str, required=True)
-			request_params = parser.parse_args()
-			print('rp=', request_params)
-		except:
-			abort(400) # bad request
+        # Parse the json
+        parser = reqparse.RequestParser()
+        try:
+             # Check for required attributes in json document, create a dictionary
+            parser.add_argument('username', type=str, required=True)
+            parser.add_argument('password', type=str, required=True)
+            request_params = parser.parse_args()
+            print('rp=', request_params)
+        except:
+            abort(400) # bad request
 
-		if 'email' in session:
-			response = {'status': 'success'}
-			responseCode = 200
-		else:
-			try:
-				ldapServer = Server(host=settings.LDAP_HOST)
-				ldapConnection = Connection(ldapServer,
-					raise_exceptions=True,
-					user='uid='+request_params['username']+', ou=People,ou=fcs,o=unb',
-					password = request_params['password'])
-				ldapConnection.open()
-				ldapConnection.start_tls()
-				ldapConnection.bind()
-				# At this point we have sucessfully authenticated.
-				session['email'] = request_params['username']+'@unb.ca'
-				response = {'status': 'success' }
-				responseCode = 201
-			except LDAPException:
-				response = {'status': 'Access denied'}
-				responseCode = 403
-			finally:
-				ldapConnection.unbind()
+        if 'email' in session:
+            response = {'status': 'success'}
+            responseCode = 200
+        else:
+            try:
+                ldapServer = Server(host=settings.LDAP_HOST)
+                ldapConnection = Connection(ldapServer,
+                    raise_exceptions=True,
+                    user='uid='+request_params['username']+', ou=People,ou=fcs,o=unb',
+                    password = request_params['password'])
+                ldapConnection.open()
+                ldapConnection.start_tls()
+                ldapConnection.bind()
+                # At this point we have sucessfully authenticated.
+                session['email'] = request_params['username']+'@unb.ca'
+                response = {'status': 'success' }
+                responseCode = 201
+            except LDAPException:
+                response = {'status': 'Access denied'}
+                responseCode = 403
+            finally:
+                ldapConnection.unbind()
 
-		return make_response(jsonify(response), responseCode)
+        return make_response(jsonify(response), responseCode)
 
-	# GET: Check Cookie data with Session data
-	#
-	# curl -i -H "Content-Type: application/json" -X GET -c cookie-jar -b cookie-jar -k https://cs3103.cs.unb.ca:5045/signin
-	def get(self):
-		success = False
-		if 'email' in session:
-			response = {'status': 'success'}
-			responseCode = 200
-		else:
-			response = {'status': 'not logged in'}
-			responseCode = 403
+    # GET: Check Cookie data with Session data
+    #
+    # curl -i -H "Content-Type: application/json" -X GET -c cookie-jar -b cookie-jar -k https://cs3103.cs.unb.ca:5045/signin
+    def get(self):
+        success = False
+        if 'email' in session:
+            response = {'status': 'success'}
+            responseCode = 200
+        else:
+            response = {'status': 'not logged in'}
+            responseCode = 403
 
-		return make_response(jsonify(response), responseCode)
+        return make_response(jsonify(response), responseCode)
 
-	# DELETE: Check Cookie data with Session data
-	#
-	# curl -i -H "Content-Type: application/json" -X DELETE -c cookie-jar -b cookie-jar -k https://cs3103.cs.unb.ca:5045/signin
+    # DELETE: Check Cookie data with Session data
+    #
+    # curl -i -H "Content-Type: application/json" -X DELETE -c cookie-jar -b cookie-jar -k https://cs3103.cs.unb.ca:5045/signin
 
-	def delete(self):
-		# print('sid=',session.sid,'session=',session)
-		if 'email' in session:
-			session.pop('email',None)
-			session.pop('admin_status',None)
-		print('del sess=',session)
-		response = {'status': 'success'}
-		responseCode = 200
-		return make_response(jsonify(response), responseCode)
+    def delete(self):
+        # print('sid=',session.sid,'session=',session)
+        if 'email' in session:
+            session.pop('email',None)
+            session.pop('admin_status',None)
+        print('del sess=',session)
+        response = {'status': 'success'}
+        responseCode = 200
+        return make_response(jsonify(response), responseCode)
