4349d4bd42d280f601c2041a5e9248bb80659922
tye-shutty <shuttyt@gmail.com>
2021-03-21 10:07:00 -0300

    Add folder mysql

---
+++ db/addFolder.sql
@@ -0,0 +1,27 @@
+DELIMITER //
+DROP PROCEDURE IF EXISTS addFolder //
+CREATE PROCEDURE addFolder
+(
+   /* Parameters */
+   IN folder_name_in VARCHAR(200),
+   IN folder_description_in VARCHAR(600),
+   IN parent_in INT
+)
+BEGIN
+
+   declare x int default 0;  
+   select count(*) into x from folders where folder_id=parent_in; 
+   
+   /* Do the INSERT */
+   IF (x != 0) THEN
+      INSERT INTO folders (folder_name, folder_description, parent)
+      VALUES (folder_name_in, folder_description_in, parent_in);
+      /* If the INSERT is successful, then this will return the Id for the record */
+      SELECT LAST_INSERT_ID();
+   ELSE
+      SIGNAL SQLSTATE '52711'
+      SET MESSAGE_TEXT = 'Unable to create the folder.';
+   END IF;
+
+END //
+DELIMITER ;
--- db/addUser.sql
+++ db/addUser.sql
@@ -3,16 +3,28 @@ DROP PROCEDURE IF EXISTS addUser //
 CREATE PROCEDURE addUser
 (
    /* Parameters */
-   IN email VARCHAR(200),
-   IN admin_status BOOLEAN
+   IN email_in VARCHAR(200),
+   IN admin_status_in BOOLEAN
 )
 BEGIN
-   /* Do the INSERT */
-  INSERT INTO users (email, admin_status) VALUES
-  (email, admin_status);
+   SET @y = (SELECT count(*) from users where email=email_in);
+   IF (@y = 0) THEN
+      /* Do the INSERT */
+      INSERT INTO folders (folder_name)
+      VALUES ("root");
+      /* If the INSERT is successful, then this will return the Id for the record */
+      SET @x = LAST_INSERT_ID();
+      
+      INSERT INTO users (email, admin_status, root_folder) 
+      VALUES (email_in, admin_status_in, @x);
 
-   /*ROW_COUNT() returns the number of rows updated, inserted or deleted by the preceding statement.*/
-   IF(ROW_COUNT() = 0) THEN
+      /*ROW_COUNT() returns the number of rows updated, inserted or deleted by the preceding statement.*/
+      IF(ROW_COUNT() = 0) THEN
+         DELETE FROM folders WHERE folder_id = @x;
+         SIGNAL SQLSTATE '52711'
+         SET MESSAGE_TEXT = 'Unable to create the user.';
+      END IF;
+   ELSE 
       SIGNAL SQLSTATE '52711'
       SET MESSAGE_TEXT = 'Unable to create the user.';
    END IF;
