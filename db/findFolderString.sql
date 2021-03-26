7365596b98eb288e92e1a92615b46d86b91f0a3c
tye-shutty <shuttyt@gmail.com>
2021-03-25 21:22:44 -0300

    Simplify sql

--- db/findFolderString.sql
+++ db/findFolderString.sql
@@ -7,10 +7,6 @@ CREATE PROCEDURE findFolderString
    IN search_string VARCHAR(600)
 )
 BEGIN
-   select folder_id from folders where email_in = email and (folder_description like search_string or folder_name like search_string); 
-   -- IF (search_string = "") THEN
-   --    SIGNAL SQLSTATE '52711'
-   --    SET MESSAGE_TEXT = 'No folder.';
-   -- END IF;
+   select folder_id from folders where email = email_in and (folder_description like search_string or folder_name like search_string); 
 END //
 DELIMITER ;
